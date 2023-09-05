7ba7afa12f150d02a99e4511e0bc129c234d1be3
Pantoja <jvpantojamaciel@gmail.com>
2023-09-05 16:38:36 -0300

    include react icons

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,8 +1,14 @@
-function Footer({}) {
-    return(
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
+function Footer({ }) {
+    return (
         <footer>
-            Rodapé
+            <ul className={styles.social_list}>
+                <li><FaFacebook></FaFacebook></li>
+                <li><FaInstagram></FaInstagram></li>
+                <li><FaLinkedin></FaLinkedin></li>
+            </ul>
         </footer>
     )
 }
-export default Footer
+export default Footer 
