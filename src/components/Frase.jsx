633c51458781835dcd10525ca1556d5c9a007192
Pantoja <jvpantojamaciel@gmail.com>
2023-09-11 15:46:47 -0300

    teste

--- src/components/Frase.jsx
+++ src/components/Frase.jsx
@@ -5,6 +5,7 @@ function Frase() {
     return (
         <div className={styles.FraseContainer}>
             <p className={styles.FraseContent}>Minha pequena frase</p>
+            <p>teste</p>
         </div>
     )
 
